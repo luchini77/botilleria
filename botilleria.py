98b2598540f5b52ffa0cfbd33ff04331eb153b17
Luis Valenzuela <luchinilvp@gmail.com>
2018-11-30 22:40:25 -0300

    segunda mejora,ahora se mejoro el metodo str

--- botilleria.py
+++ botilleria.py
@@ -1,8 +1,5 @@
 class Botilleria:
 
-    cerveza = ""
-    vino = ""
-    agua = ""
 
     def __init__(self,marca,litros,precio):
         self.marca = marca
@@ -10,23 +7,31 @@ class Botilleria:
         self.precio = precio
 
     def __str__(self):
-        return "{} {} de {} litro, tiene un precio de {} pesos.".format(self.cerveza,self.marca,self.litros,self.precio)
-
+        return "{} de {} litro, tiene un precio de {} pesos.".format(self.marca,self.litros,self.precio)
 
 
 class Cerveza(Botilleria):
 
-    cerveza = "Cerveza"
+    tipo = ""
+
+    def __str__(self):
+        return "{} {} de {} litro, tiene un precio de {} pesos.".format(self.tipo,self.marca,self.litros,self.precio)
 
 
 
 class Vino(Botilleria):
 
-    vino = "Vino"
+    tipo = ""
+
+    def __str__(self):
+        return "{} ,{} de {} litro, tiene un precio de {} pesos.".format(self.tipo,self.marca,self.litros,self.precio)
 
 
 
 class Aguas(Botilleria):
 
-    agua = "Agua"
+    tipo = ""
+
+    def __str__(self):
+        return "{} {} de {} litro, tiene un precio de {} pesos.".format(self.tipo,self.marca,self.litros,self.precio)
 
--- principal.py
+++ principal.py
@@ -1,11 +1,21 @@
 import botilleria
 
 cerveza = botilleria.Cerveza("Cristal",1,1500)
+cerveza.tipo = "Cerveza"
 vino = botilleria.Vino("Gato Negro",1.5,2500)
+vino.tipo = "Vino Tinto"
 agua = botilleria.Aguas("Aloe Vera",1,1200)
+agua.tipo = "Agua"
+nectar = botilleria.Aguas("Andina",1.5,900)
+nectar.tipo = "Nectar"
+
+
+
 print("***********************************************************")
 print(cerveza)
 print("***********************************************************")
 print(vino)
 print("***********************************************************")
 print(agua)
+print("***********************************************************")
+print(nectar)
